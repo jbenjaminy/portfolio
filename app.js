4dd1d15361505ae83d685391738e71ec9ae50522
jbenjaminy <young.benjamin@gmail.com>
2016-11-15 01:10:25 -0600

    adds page scrolls to tabs and on refresh

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 'use strict';
 $(document).ready(function() {
+	$(this).scrollTop(0);
+
 	$('.about-tab').on('click', function(event) {
 		$('.page').hide();
 		$('.about').show();
 	});
+
 	$('.portfolio-tab').on('click', function(event) {
 		$('.page').hide();
 		$('.portfolio').show();
 	});
+
 	$('.contact-tab').on('click', function(event) {
 		$('.page').hide();
 		$('.contact').show();
+	});
+
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+		var target = this.hash;
+	    var $target = $(target);
+		$('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing');
 	});
 
 	$('.proj-image').mouseenter(function() {
@@ -18,6 +31,7 @@ $(document).ready(function() {
 		$('.hover-image').show();
 		$('.description').show();
 	})
+
 	$('.proj-image').mouseleave(function() {
 		$('.image').show();
 		$('.hover-image').hide();
